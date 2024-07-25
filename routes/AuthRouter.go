d3ff90308462103351a56728cbf2a728b5bbf077
Nguyen Huu Dat <datttdayne@gmail.com>
2024-07-25 23:10:23 +0700

    feat: authorization and authentication

--- routes/AuthRouter.go
+++ routes/AuthRouter.go
@@ -1,1 +1,12 @@
 package routes
+
+import(
+	controller "github.com/Dattt2k2/golang-project/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func AuthRoutes(incomingRoutes *gin.Engine){
+	incomingRoutes.POST("users/signup", controller.SignUp())
+	incomingRoutes.POST("users/login", controller.Login())
+}
+
