c272b2f949bea111ecb80ed07aa81bb918d96b76
Nguyen Huu Dat <datttdayne@gmail.com>
2024-07-25 08:41:49 +0700

    initial commit

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import(
+	routes "github.com/golang-project/routes"
+	"os"
+	"github.com/gin-gonic/gin"
+
+)	
+
+func main(){
+	port := os.Getenv("PORT")
+
+	if port == ""{
+		port = "8000"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	routes.AuthRoutes(router)
+}
