8df1866fa69ae084b6c3e77d993be7afe98cacd0
Nguyen Huu Dat <datttdayne@gmail.com>
2024-08-12 15:21:58 +0700

    fix: add product, edit product and search product

--- models/productModel.go
+++ models/productModel.go
@@ -9,7 +9,7 @@ import (
 type Product struct{
 	ID             primitive.ObjectID 		`bson:"_id,omitempty" json:"id,omitempty"`
 	Name			*string					`json:"name" validate:"required,min=2,max=100"`
-	Image			string					`json:"image"`
+	Image			primitive.Binary		`json:"image"`
 	Description		*string					`json:"description" validate:"required,min=2,max=100"`
 	Price			float64					`json:"price" validate:"required"`
 	Created_at		time.Time				`json:"created_at"`
--- routes/productManagerRouter.go
+++ routes/productManagerRouter.go
@@ -11,8 +11,14 @@ func ProductManagerRoutes(incomingRoutes *gin.Engine){
 	authorized := incomingRoutes.Group("/")
 	authorized.Use(middleware.Authenticate())
 
+	// add product to database
 	authorized.POST("/products", controller.AddProduct())
-	// authorized.PUT("/products/:id", controller.EditProduct())
+	// Edit product from database
+	authorized.PUT("/products/:id", controller.EditProduct())
+	// Delete product from databse
 	authorized.DELETE("/products/:id", controller.DeleteProduct())
+	// Get all product from database
 	authorized.GET("/products", controller.GetAllProducts())
+	// search product
+	authorized.GET("/products/search", controller.GetProdctByNameHander())
 }
