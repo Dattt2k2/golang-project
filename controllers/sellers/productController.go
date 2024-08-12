eb4c756ed3d3388e9cf43fb35d4c02bb1d417e1f
Nguyen Huu Dat <datttdayne@gmail.com>
2024-08-12 15:21:58 +0700

    fix: add product, edit product and search product

--- controllers/sellers/productController.go
+++ controllers/sellers/productController.go
@@ -2,9 +2,9 @@ package controllers
 
 import (
 	"context"
-	"encoding/json"
 	"io"
 	"net/http"
+	"strconv"
 	"time"
 
 	database "github.com/Dattt2k2/golang-project/database/databaseConnection.gp"
@@ -14,268 +14,142 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/gridfs"
-	"go.mongodb.org/mongo-driver/mongo/options"
+	// "go.mongodb.org/mongo-driver/mongo/options"
 )
 
 
 var productCollection *mongo.Collection = database.OpenCollection(database.Client, "product")
 var validate = validator.New()
 
-// func AddProduct() gin.HandlerFunc{
-// 	return func(c *gin.Context){
-// 		var product models.Product
-// 		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
-// 		if err := c.BindJSON(&product); err != nil{
-// 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-// 			return
-// 		}
-// 		validationErr := validate.Struct(product)
-// 		if validationErr != nil{
-// 			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
-// 			return
-// 		}
-		
-// 		product.Created_at = time.Now()
-// 		product.Updated_at = time.Now()
-// 		product.ID = primitive.NewObjectID()
 
-// 		result, err := productCollection.InsertOne(ctx, product)
-// 		if err != nil{
-// 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-// 			return
-// 		}
-// 		c.JSON(http.StatusOK, gin.H{"id": result.InsertedID})
-// 		defer cancel()
-// 	}
-// }
-
-// var db *mongo.Database 
 
 func AddProduct() gin.HandlerFunc{
-	return func(c *gin.Context){
+	return func(c *gin.Context) {
 		var product models.Product
 		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
-		
-		if err:= c.ShouldBindJSON(&product); err!= nil{
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
 
-		validationErr := validate.Struct(product)
-		if validationErr != nil{
-			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
-			return
-		}
 
-		file, header, err := c.Request.FormFile("image")
+
+		name := c.PostForm("name")
+		description := c.PostForm("description")
+		price := c.PostForm("price")
+
+		file, err := c.FormFile("image")
 		if err != nil{
-			c.JSON(http.StatusBadRequest, gin.H{"error": "Failed to upload image"})
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Image is required"})
 			return
 		}
-		defer file.Close()
 
-		data, err := io.ReadAll(file)
+		fileContent, err := file.Open()
 		if err != nil{
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to read all file"})
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to read image"})
 			return
 		}
-		
-		bucket, err := gridfs.NewBucket(
-			productCollection.Database(),
-			options.GridFSBucket().SetName("images"),
-		)
+
+		defer fileContent.Close()
+
+		imageBytes, err := io.ReadAll(fileContent)
 		if err != nil{
-			c.JSON(http.StatusBadRequest, gin.H{"error": "Failed to create Gridfs bucket"})
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to read image content"})
 			return
 		}
 
-		uploadStream, err := bucket.OpenUploadStream(header.Filename)
-		if err != nil{
-			c.JSON(http.StatusBadRequest, gin.H{"error": "Failed to upload stream"})
-			return
-		}
-		defer uploadStream.Close()
-
-		_, err = uploadStream.Write(data)
-		if err != nil{
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to write file to GridFs"})
-			return
-		}
-
-		fileID, ok := uploadStream.FileID.(primitive.ObjectID)
-		if !ok{
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to convet FildId to ObjectId"})
-			return
-		}
-
-		//get userID
-		userID := c.GetString("uid")
+		userID:= c.GetString("uid")
 		if userID == ""{
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to get userID"})
 			return
 		}
 
+		product.Name = &name
+		product.Description = &description
+		product.Image = primitive.Binary{Data: imageBytes}
+		product.Price = parseFloat(price)
 		product.UserID, _ = primitive.ObjectIDFromHex(userID)
-		product.Image = fileID.Hex()
-		product.Created_at = time.Now()
-		product.Updated_at = time.Now()
-		product.ID = primitive.NewObjectID()
 
-		result, err := productCollection.InsertOne(ctx, product)
-		if err != nil{
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		if err := validate.Struct(product); err != nil{
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
 
+		now := time.Now()
+		product.Created_at = now
+		product.Updated_at = now
 
-		c.JSON(http.StatusOK, gin.H{"id": result.InsertedID})
+		product.ID = primitive.NewObjectID()
+
+		_, err = productCollection.InsertOne(ctx, product)
+		if err != nil{
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to add product"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"message": "Product added successfully", "product": product.Name})
 		defer cancel()
 	}
 }
 
-// func EditProduct() gin.HandlerFunc{
-// 	return func(c *gin.Context){
-// 		var product models.Product
-// 		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
-// 		productId := c.Param("id")
+func parseFloat(s string) float64{
+	f, _ := strconv.ParseFloat(s, 64)
+	return f
+}
 
-// 		objID, _ := primitive.ObjectIDFromHex(productId)
-
-// 		if err := c.BindJSON(&product); err !=nil{
-// 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-// 			return
-// 		}
-
-// 		validationErr := validate.Struct(product)
-// 		if validationErr != nil{
-// 			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
-// 			return
-// 		}
-
-// 		product.Updated_at = time.Now()
-
-// 		update := bson.M{
-// 			"name":			product.Name,
-// 			"description":	product.Description,
-// 			"price":		product.Price,
-// 			"image":		product.Image,
-// 			"updated_at":	product.Updated_at,
-// 			"user_id":		product.UserID,
-// 		}
-
-// 		resutl, err := productCollection.UpdateOne(ctx, bson.M{"_id": objID}, bson.M{"$set": update})
-// 		if err != nil{
-// 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-// 			return
-// 		}
-		
-// 		if resutl.MatchedCount == 0{
-// 			c.JSON(http.StatusNotFound, gin.H{"error":"Product not found"})
-// 			return
-// 		}
-
-// 		c.JSON(http.StatusOK, gin.H{"message": "Product update successful"})
-// 		defer cancel()
-// 	}
-// }
 
 func EditProduct() gin.HandlerFunc{
-	return func(c *gin.Context) {
+	return func (c *gin.Context)  {
+		// var product models.Product
 		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 
-		productID := c.Param("id")
-
-		objID, err := primitive.ObjectIDFromHex(productID)
+		productID, err := primitive.ObjectIDFromHex(c.Param("id"))
 		if err != nil{
-			c.JSON(http.StatusBadRequest, gin.H{"errpr": "Invalid product"})
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid product id"})
 			return
 		}
 
-		form, err := c.MultipartForm()
+		name := c.PostForm("name")
+		description := c.PostForm("description")
+		price := c.PostForm("price")
+
+
+		update := bson.M{"updated_at": time.Now()}
+		
+		if name != ""{
+			update["name"] = name
+		}
+
+		if description != ""{
+			update["description"] = description
+		}
+
+		if price != ""{
+			update["price"] = parseFloat(price)
+		}
+
+
+		file, err := c.FormFile("image")
+		if err == nil{
+			fileContent, err := file.Open()
+			if err != nil{
+				c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to read image"})
+				return
+			}
+			defer fileContent.Close()
+			imageBytes, err := io.ReadAll(fileContent)
+			if err != nil{
+				c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to read image content"})
+				return
+			}
+
+			update["image"] = primitive.Binary{Data: imageBytes}
+		}
+
+		result, err := productCollection.UpdateOne(
+			ctx,
+			bson.M{"_id": productID},
+			bson.M{"$set": update},
+		)
+
 		if err != nil{
-			c.JSON(http.StatusBadRequest, gin.H{"error": "Failed to parse form"})
-			return
-		}
-
-		var product models.Product
-		productData := form.Value["product"]
-		if len(productData) > 0{
-			if err := json.Unmarshal([]byte(productData[0]), &product); err != nil{
-				c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid product data form"})
-				return
-			}
-		}
-
-		validationErr := validate.Struct(product)
-		if validationErr != nil{
-			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
-			return
-		}
-
-		if fileHeaders, ok := form.File["image"]; ok && len(fileHeaders) > 0 {
-			fileHeader := fileHeaders[0]
-			file, err := fileHeader.Open()
-			if err != nil {
-				c.JSON(http.StatusBadRequest, gin.H{"error": "Failed to open file"})
-				return
-			}
-			defer file.Close()
-
-			data, err := io.ReadAll(file)
-			if err != nil {
-				c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to read file"})
-				return
-			}
-
-			// Tạo bucket GridFS
-			bucket, err := gridfs.NewBucket(
-				productCollection.Database(),
-				options.GridFSBucket().SetName("images"),
-			)
-			if err != nil {
-				c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create GridFS bucket"})
-				return
-			}
-
-			// Tạo upload stream
-			uploadStream, err := bucket.OpenUploadStream(fileHeader.Filename)
-			if err != nil {
-				c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create upload stream"})
-				return
-			}
-			defer uploadStream.Close()
-
-			// Ghi dữ liệu vào GridFS
-			if _, err := uploadStream.Write(data); err != nil {
-				c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to write file to GridFS"})
-				return
-			}
-
-			// Lấy fileID từ uploadStream
-			fileID, ok := uploadStream.FileID.(primitive.ObjectID)
-			if !ok {
-				c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to convert file ID to ObjectID"})
-				return
-			}
-
-			// Cập nhật ảnh trong sản phẩm
-			product.Image = fileID.Hex()
-		}
-
-		product.Updated_at = time.Now()
-		update := bson.M{
-			"name":				product.Name,
-			"description":		product.Description,
-			"price":			product.Price,
-			"image":			product.Image,
-			"updated_at":		product.Updated_at,
-			"user_id":			product.UserID,
-		}
-
-		result, err := productCollection.UpdateOne(ctx, bson.M{"_id": objID}, bson.M{"$set": update})
-		if err != nil{
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update product"})
 			return
 		}
 
@@ -284,10 +158,12 @@ func EditProduct() gin.HandlerFunc{
 			return
 		}
 
-		c.JSON(http.StatusOK, gin.H{"message": "Product updated successful"})
+		c.JSON(http.StatusOK, gin.H{"message": "Product updated successfully"})
 		defer cancel()
 	}
 }
+
+
 
 func DeleteProduct() gin.HandlerFunc{
 	return func(c *gin.Context){
