352ecd3f3b6229ce8302939d74a6963dc93ee666
Dattt <97326252+Dattt2k2@users.noreply.github.com>
2024-09-10 16:39:36 +0700

    feat:delete product from cart and get product from cart (#5)

--- controllers/users/cartController.go
+++ controllers/users/cartController.go
@@ -15,7 +15,7 @@ import (
 )
 
 var cartCollection *mongo.Collection = database.OpenCollection(database.Client, "cart")
-
+var productCollection *mongo.Collection = database.OpenCollection(database.Client, "product")
 
 func AddToCart() gin.HandlerFunc{
 	return func(c *gin.Context) {
@@ -78,10 +78,55 @@ func GetCart() gin.HandlerFunc{
 	}
 }
 
-// func DeleteProductFromCart() gin.HandlerFunc{
-// 	return func (c *gin.Context)  {
-// 		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+func DeleteProductFromCart() gin.HandlerFunc{
+	return func (c *gin.Context)  {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+
+		productId := c.Param("id")
+
+		objId, _ := primitive.ObjectIDFromHex(productId)
+
+		result, err := cartCollection.DeleteOne(ctx, bson.M{"_id": objId})
+		if err != nil{
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Failed to delete product from cart"})
+			return
+		}
+		if result.DeletedCount == 0{
+			c.JSON(http.StatusNotFound, gin.H{"error": "Product not found"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"message": "Delete product from cart successful"})
+		defer cancel()
 
 		
-// 	}
-// }
+	}
+}
+
+func GetProductFromCart() gin.HandlerFunc{
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		
+		var product []models.Product
+		productId := c.Param("id")
+		objId, _ := primitive.ObjectIDFromHex(productId)
+
+		filter := bson.D{{"_id", objId}}
+		
+		cursor, err := productCollection.Find(ctx, filter)
+
+		if err != nil{
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to get product"});
+			return
+		}
+		defer cursor.Close(ctx)
+
+		if err := cursor.All(ctx, &product); err != nil{
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to decode data"})
+			return
+		}
+		defer cancel()
+
+		c.JSON(http.StatusOK, product)
+	}
+}
