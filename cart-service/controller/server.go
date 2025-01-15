4503919096a2ae7a753be595ed5f3f638642a281
Nguyen Huu Dat <datttne484@gmail.com>
2025-01-15 22:07:38 +0700

    test CI

--- cart-service/controller/server.go
+++ cart-service/controller/server.go
@@ -1,15 +1,32 @@
 package controller
 
 import (
-	"context"
+	// "context"
+	// "log"
 
+	// "github.com/Dattt2k2/golang-project/cart-service/models"
 	pb "github.com/Dattt2k2/golang-project/module/gRPC-cart/service"
+	// "go.mongodb.org/mongo-driver/bson/primitive"
+	// "google.golang.org/grpc/codes"
+	// "google.golang.org/grpc/status"
 )
 
 type CartServer struct{
 	pb.UnimplementedCartServiceServer
 }
 
-func (s *CartServer) GetCartItems(ctx context.Context, req * pb.CartRequest) (*pb.CartResponse, error){
-	id := re
-}
+// func (s *CartServer) GetCartItems(ctx context.Context, req * pb.CartRequest) (*pb.CartResponse, error){
+// 	id := req.UserId
+
+// 	productID, err := primitive.ObjectIDFromHex(id)
+// 	if err != nil{
+// 		return nil, status.Errorf(codes.InvalidArgument, "Invalid product ID formate: %v", err)
+
+// 	}
+
+// 	log.Printf("product id: %v", productID)
+
+// 	var cart models.CartItem
+
+// 	if err := cartCollection.FindOne()
+// }
